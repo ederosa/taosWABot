0f718d9e44695f9b4872a508cf5607979fc559b8
ederosa <ederosa@mpf.gov.ar>
2025-01-29 19:47:56 -0300

    Script para correr chequeo de mails

---
+++ chequeamails.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ruta al archivo que deseas ejecutar
+SCRIPT_PATH="/home/taospi/git/taosWABot/gmailbot.js"
+
+# Verificar si el archivo existe
+if [ -f "$SCRIPT_PATH" ]; then
+  echo "Ejecutando $SCRIPT_PATH..."
+  # Ejecutar el script con Node.js
+  /home/taospi/git/taosWABot/node "$SCRIPT_PATH"
+else
+  echo "Error: El archivo $SCRIPT_PATH no existe."
+  exit 1
+fi
