42168f6a03ca3292f7ffd1e9999dcee263d86de2
Enrique De Rosa <taos78@gamil.com>
2025-01-29 19:55:25 -0300

    Modificamos path de script

--- chequeamails.sh
+++ chequeamails.sh
@@ -7,7 +7,7 @@ SCRIPT_PATH="/home/taospi/git/taosWABot/gmailbot.js"
 if [ -f "$SCRIPT_PATH" ]; then
   echo "Ejecutando $SCRIPT_PATH..."
   # Ejecutar el script con Node.js
-  node "$SCRIPT_PATH"
+  cd /home/taospi/git/taosWABot && node "$SCRIPT_PATH"
 else
   echo "Error: El archivo $SCRIPT_PATH no existe."
   exit 1
