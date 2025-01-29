4d22f645f6a3462d76efdd9a3f46ddf788f64e19
ederosa <ederosa@mpf.gov.ar>
2025-01-29 17:42:23 -0300

    Commit del branch de prueba de acceso a Gmail

---
+++ gmailbot.js
@@ -0,0 +1,179 @@
+// Referencia
+// https://developers.google.com/gmail/api/quickstart/nodejs?hl=es-419
+
+require('dotenv').config();
+
+
+const userID = '5491150100910@s.whatsapp.net';
+const { convert } = require('html-to-text');
+const options = {
+  wordwrap: 130,
+};
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+const { auth } = require('googleapis/build/src/apis/gkehub');
+const condition = process.env.CONDITION;
+
+const huggingface_facebook = process.env.HUGGINGFACE_FACEBOOK
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listLabels(auth) {
+  const gmail = google.gmail({ version: 'v1', auth });
+  const res = await gmail.users.labels.list({
+    userId: 'me',
+  });
+  const labels = res.data.labels;
+  if (!labels || labels.length === 0) {
+    console.log('No labels found.');
+    return;
+  }
+  //console.log('Labels:');
+  labels.forEach((label) => {
+    console.log(`- ${label.name}`);
+  });
+}
+
+/**
+ * Lists the messages in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listMessages(auth, condition) {
+  const gmail = google.gmail({ version: 'v1', auth });
+  const res = await gmail.users.messages.list({
+    userId: 'me',
+    maxResults: 100,
+    pageToken: '',
+    q: condition
+  });
+  return res.data.messages;
+}
+
+async function processMessages(auth, messages) {
+  // console.log(messages);
+  let mensajesProcesados = []
+  for (const message of messages) {
+    let summarizedMessage = await getMessage(auth, message.id)
+    mensajesProcesados.push(summarizedMessage)
+    // console.log(summarizedMessage)
+  }
+  return mensajesProcesados
+}
+
+async function getMessage(auth, messageId) {
+  // console.log(messageId)
+  const gmail = google.gmail({ version: 'v1', auth });
+  const res = await gmail.users.messages.get({
+    userId: 'me',
+    id: messageId
+  });
+  //  console.log(res.data.payload.mimeType)
+  let mensaje = "";
+  if (res.data.payload.mimeType == 'text/html')
+    mensaje = convert(Buffer.from(res.data.payload.body.data, 'base64').toString());
+  else if (res.data.payload.mimeType == 'text/plain')
+    mensaje = convert(Buffer.from(res.data.payload, 'base64').toString());
+  else if (res.data.payload.mimeType == 'multipart/alternative'
+    || res.data.payload.mimeType == 'multipart/alternaitve')
+    mensaje = Buffer.from(res.data.payload.parts[0].body.data, 'base64').toString();
+  else if (res.data.payload.mimeType == 'multipart/mixed')
+    mensaje = Buffer.from(res.data.payload.parts[0].parts[0].body.data, 'base64').toString();
+
+  const text = await query({ "inputs": mensaje })
+  //  console.log(text)
+  return text
+}
+
+async function query(data) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+    {
+      headers: {
+        Authorization: huggingface_facebook,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
+
+async function initiate(condition) {
+  const client = await authorize();
+  const messages = await listMessages(client, condition)
+  const mensajesProcesados = await processMessages(client, messages)
+  console.log(mensajesProcesados)
+}
+
+//authorize().then(listLabels).catch(console.error);
+
+initiate(condition)
---
+++ plugins/flowMails.js
@@ -0,0 +1,95 @@
+const { addKeyword } = require('@bot-whatsapp/bot')
+
+// Agrega cero si es menor de 10
+function addZeroToDate(value) {
+  if (Number(value) < 10)
+    return '0' + Number(value);
+  return value
+}
+
+// Formatea la fecha
+function getDate(rawDate) {
+
+  // Buscamos el precio con una regex.
+  let re = new RegExp(process.env.REGEXDATE);
+  let match = re.exec(rawDate);
+  let fecha = []
+
+  // Y hay match con la regex la distribuye en el array
+  if (match != null) {
+
+    fecha.push(addZeroToDate(match[1]));
+    fecha.push(addZeroToDate(match[2]));
+    fecha.push([match ? match[3] : null]);
+
+  } else {
+    // Si la fecha no tiene formato correcto pone fecha de hoy.
+    const date = new Date();
+
+    fecha.push(date.getDate());
+    fecha.push(date.getMonth() + 1);
+    fecha.push(date.getFullYear());
+  }
+
+  return fecha;
+
+}
+
+const flowMails = addKeyword(['gastos', 'gasto'])
+
+  .addAnswer('Hola, intentaremos cargar el gasto!', null, async (ctx) => {
+
+    // Número de celular
+    const numeroDeWhatsapp = ctx.from;
+
+    // Verificamos que estén mandando el mensaje de un número válido.
+    if (process.env.WHITELIST.split(",").includes(numeroDeWhatsapp)) {
+
+      // Construímos un array con los valores recibidos en el mensaje.
+      const mensajeRecibido = ctx.body.split(" ");
+      // Si el mensaje tiene entre 3 y 4 palabras pasa.
+      if (mensajeRecibido.length >= process.env.CANTMINPARAM
+        && mensajeRecibido.length <= process.env.CANTPARAM) {
+
+        const formattedDate = getDate(mensajeRecibido[1]);
+
+        // Obtiene la primera y segunda columna.
+        const sheetFirstColumn = process.env.PRIMERACOLMESES.split(",")[Number(formattedDate[1]) - 1];
+        const sheetSecondColumn = process.env.SEGUNDACOLMESES.split(",")[Number(formattedDate[1]) - 1];
+
+        const sheetRow = process.env.SHEETROW;
+        const sheetRange = process.env.SHEETRANGE;
+
+        // Obtenemos columna a insertar  y calculamos la última celda.
+        const reader = await googleSheets.read(process.env.SPREADSHEETID, `${sheetRange}!${sheetFirstColumn}${sheetRow}:${sheetFirstColumn}`);
+
+        // Obtenemos la fila de la primera celda sin valores
+        const ultimaFila = reader.length + Number(sheetRow);
+
+        // Esta variable nos va a indicar qué posición del array de parámetros vamos a tomar.
+        let initialPos = 1;
+
+        // Armamos el array para escribir en la planilla de acuerdo 
+        // a la cantidad de paràmetros ingresados por el usuario.
+        if (mensajeRecibido.length == process.env.CANTPARAM)
+          initialPos = initialPos + 1;
+
+        // Valores a poner en las celdas
+        const values = [[formattedDate[0] + " - " + mensajeRecibido[initialPos], mensajeRecibido[initialPos + 1]]];
+
+        try {
+          // Llamamos a la función para escribir en la celda con los valores que definimos. 
+          const writer = googleSheets.write(process.env.SPREADSHEETID,
+            //process.env.RANGE,
+            `${sheetRange}!${sheetFirstColumn}${ultimaFila}:${sheetSecondColumn}${ultimaFila}`,
+            values,
+            'USER_ENTERED');
+          console.log("Gasto cargado!")
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  })
+
+module.exports = { flowMails };
